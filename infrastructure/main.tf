541b6ebe9db7d9dce53b052c77671250623424c1
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2024-11-11 18:04:28 -0500

    S3 - Ignore public ACLs and restrict public buckets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,10 +18,12 @@ locals {
 
 ### S3 bucket - static website hosting ###
 module "s3_website" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-  bucket = "${local.project_name}-static-website"
-  block_public_acls = false
-  block_public_policy = false
+  source                  = "terraform-aws-modules/s3-bucket/aws"
+  bucket                  = "${local.project_name}-static-website"
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
   website = {
     index_document = "index.html"
   }
@@ -32,7 +34,7 @@ resource "aws_s3_object" "index_html" {
   bucket = module.s3_website.s3_bucket_id
   key    = "index.html"
   source = "../static-website/index.html"
-  acl = "public-read"
+  acl    = "public-read"
 }
 
 ### S3 bucket - upload/source ###

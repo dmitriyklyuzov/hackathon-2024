431bd2892fba40a338ee7fd6d835c7f8f2d5761a
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2024-11-11 17:13:31 -0500

    added the static website s3 bucket

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,7 +12,29 @@ provider "aws" {
   profile = "default" # update this later if needed
 }
 
+locals {
+  project_name = "calcurator"
+}
+
 ### S3 bucket - static website hosting ###
+module "s3_website" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket = "${local.project_name}-static-website"
+  block_public_acls = false
+  block_public_policy = false
+  website = {
+    index_document = "index.html"
+  }
+}
+
+### Static website source code
+resource "aws_s3_object" "index_html" {
+  bucket = module.s3_website.s3_bucket_id
+  key    = "index.html"
+  source = "../static-website/index.html"
+  acl = "public-read"
+}
+
 ### S3 bucket - upload/source ###
 ### S3 bucket - destination/result ###
 ### Lambda ###
